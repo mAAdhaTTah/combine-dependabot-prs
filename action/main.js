d1991b4982b7f7fd55068b3a64d9bf8b644f906b
mAAdhaTTah <jamesorodig@gmail.com>
2021-06-13 00:18:41 -0400

    Add CLI
    Clean up logic to extract only reusable logic into the module itself.
    The CLI takes advantage of the same basic logic, interfacing with
    the input with `meow`. Make major improvements in both the internal
    logic as well as the logging & dependency injection.

---
+++ action/main.js
@@ -0,0 +1,64 @@
+const core = require("@actions/core");
+const { context, getOctokit } = require("@actions/github");
+const execa = require("execa");
+const { combinePRs } = require("../lib");
+
+/**
+ * @param {any} err
+ */
+const handleError = (err) => {
+  core.error(err);
+  core.setFailed(`Unhandled error: ${err}`);
+};
+
+process.on("unhandledRejection", handleError);
+
+const main = async () => {
+  const githubToken = core.getInput("githubToken", { required: true });
+  const mustBeGreen = core.getBooleanInput("mustBeGreen", { required: true });
+  const branchPrefix = core.getInput("branchPrefix", { required: true });
+  const ignoreLabel = core.getInput("ignoreLabel", { required: true });
+  const combineBranchName = core.getInput("combineBranchName", {
+    required: true,
+  });
+  const baseBranch = core.getInput("baseBranch", { required: true });
+  const openPR = core.getBooleanInput("openPR", { required: true });
+  const github = getOctokit(githubToken);
+
+  await execa("git", ["config", "user.name", "github-actions"]);
+  await execa("git", ["config", "user.email", "github-actions@github.com"]);
+
+  await combinePRs(
+    {
+      github,
+      target: context.repo,
+      logger: {
+        info(msg) {
+          core.info(msg);
+        },
+        success(msg) {
+          core.info(msg);
+        },
+        warning(msg) {
+          core.warning(msg);
+        },
+        error(msg) {
+          core.error(msg);
+        },
+        group(name, cb) {
+          return core.group(name, cb);
+        },
+      },
+    },
+    {
+      mustBeGreen,
+      branchPrefix,
+      ignoreLabel,
+      combineBranchName,
+      baseBranch,
+      openPR,
+    }
+  );
+};
+
+main().catch(handleError);
